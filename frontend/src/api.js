79f1097c812113fb388870536fa47fa566f63abe
unfit-gokul007 <gokulkathick984@gmail.com>
2026-02-06 15:16:30 +0530

    fix api base url add /api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mern-notes-backend-qzno.onrender.com",
+  baseURL: "https://mern-notes-backend-qzno.onrender.com/api",
 });
 
 // Automatically add token to every request
