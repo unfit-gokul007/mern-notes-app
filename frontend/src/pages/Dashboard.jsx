0c7464e4942c76c1ff1708cd92bad98f881c6734
unfit-gokul007 <gokulkathick984@gmail.com>
2026-02-11 15:19:06 +0530

    Add search notes feature (search by title and content)

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -127,7 +127,7 @@ const logout = () => {
 </li>
 
 
-            <div style={{paddingLeft:"98px"}}> Your Notes
+            <div style={{paddingLeft:"40px"}}> Your Notes
     <NotesList notes={filteredNotes} onSelect={setSelectedNote}/>
       </div>
   </div>
